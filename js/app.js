dc0e78eec229cfcd53b310a67c5ad1219b219695
ViktoryDem <36764620+ViktoryDem@users.noreply.github.com>
2025-01-25 18:43:03 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const root = document.documentElement;
+
+  root.style.setProperty("--main-color", mainColor);
+  root.style.setProperty("--secondary-color", secondaryColor);
+});
